2f08c27ccf4af46af4688032d0af6e13914830c0
jkbkrh <138328520+jkbkrh@users.noreply.github.com>
2023-11-05 06:16:23 +0100

    add ng-mocks and use it in test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,24 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MockBuilder, MockRender } from 'ng-mocks';
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
-  beforeEach(() =>
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
-    }),
-  );
+  beforeEach(() => MockBuilder(AppComponent, AppModule));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = MockRender(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'angular-starter'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = MockRender(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-starter');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = MockRender(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
